322cef56c1344a3edbcc18824f176c2ef2e80af5
Franklin Araújo <franklinarauj@sempreceub.com>
2021-11-08 04:35:42 -0300

    Dados

---
+++ src/components/EdicaoDado.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <v-form>
+            <v-card class="mb-2">
+
+                <v-card-text class="pa-5">
+                    <v-row>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-select :items="perfis" filled label="Perfil" v-model="perfis[0]" />
+                        </v-col>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-select :items="ufs" filled label="Estado/UF" v-model="ufs[6]" />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-text-field filled label="Nome do Usuário" v-model="nome[0]" />
+                        </v-col>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-text-field filled label="E-mail do Usuário" v-model="email[0]" />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-text-field filled label="Telefone" v-mask="'(##) ####-####'" v-model="telefone[0]" />
+                        </v-col>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-text-field filled label="Celular" v-mask="'(##) #####-####'" v-model="celular[0]" />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col class="d-flex" cols="12" sm="4">
+                            <v-text-field filled v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'"
+                                name="input-10-1" label="Senha" hint="No mínimo 6 caracteres." counter
+                                @click:append="show1 = !show1"></v-text-field>
+                        </v-col>
+                        <v-col class="d-flex" cols="12" sm="4">
+                            <v-text-field filled v-model="passwordConfirm"
+                                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]"
+                                :type="show2 ? 'text' : 'password'" name="input-10-1" label="Confirmação de Senha"
+                                hint="No mínimo 6 caracteres." counter @click:append="show2 = !show2"></v-text-field>
+                        </v-col>
+                        <v-col class="d-flex" cols="12" sm="4">
+                            <v-select :items="situacao" filled label="Situação" v-model="situacao[0]" />
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+
+                <div class="d-flex justify-end pa-5">
+                    <v-btn class="mr-4">
+                        Voltar
+                    </v-btn>
+                    <v-btn color="primary">
+                        Salvar
+                    </v-btn>
+                </div>
+
+            </v-card>
+        </v-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            nome: ['Gislane Santana'],
+            email: ['gislane.santana@email.com.br'],
+            telefone: ['61123456789'],
+            celular: ['61987654321'],
+            perfis: ['Administrador', 'Cliente',
+                'Cliente Master', 'Consultor', 'Consultor Master'
+            ],
+            ufs: [
+                "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO",
+                "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI",
+                "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+            ],
+            situacao: ["Ativo", "Inativo"],
+            show1: false,
+            show2: false,
+            password: 'senha123',
+            passwordConfirm: 'senha123',
+            rules: {
+                required: value => !!value || 'Obrigatório.',
+                min: v => v.length >= 6 || 'Mínimo 6 caracteres.'
+            },
+        }),
+    }
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Grupos from '../views/Grupos';
 import Usuarios from '../views/Usuarios';
 import Diagnosticos from '../views/Diagnosticos';
 import Respostas from '../views/Respostas';
+import Dados from '../views/Dados';
 
 import Login from '../views/Login.vue';
 
@@ -42,6 +43,10 @@ const routes = [
       {
         path: '/respostas',
         component: Respostas,
+      },
+      {
+        path: '/dados',
+        component: Dados,
       },
     ]
   }
